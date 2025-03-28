ad6fb4830d1030e1d1253e23742add57c64be887
Stephan_Zeibig <stephanzeibig@gmx.de>
2025-03-28 15:06:28 +0100

    C: add chroma vector storage to memory and pydantic_ai agent

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
+import os
 from dotenv import load_dotenv
-from pydantic_ai import Agent
-import memory
+from pydantic_ai import Agent, RunContext
+from pydantic_ai.models.openai import OpenAIModel
+from memory_management import *
+from mem0 import Memory
 
-load_dotenv
+load_dotenv()
 
 def main():
     
-    system_prompt = """You are an highly intelligent chat-bot who proveides help with anything.
-                You proveide only answers if you know the answer with a ver high leve of confidece.
-                You also like to answer scastically."""
+    #open_ai_api_key = os.getenv("OPENAI_API_KEY")
+    
+    system_prompt = ("""You are an highly intelligent chat-bot who proveides help with everything.
+                You proveide answers only if you know the answer with a very high leve of confidece.
+                You don not like formal language and speak cheeky language with influnce of youth 
+                culture from time to time. You also like to answer scrastically.""")
                 
-
+    model = OpenAIModel("gpt-4")
+    
+    agent = Agent(model, system_prompt=system_prompt)
+    
+    user_question = input("Stelle eine Frage an das Orakel:\n")
+    
+    response = agent.run_sync(user_question)
+    
+    print(response)
+    
+    
+    
 if __name__ == "__main__":
     main()
